1a064211376f4552d0807aa980e513e8fe8a148b
Kasidate P <bank@kasidate.me>
2022-03-25 12:08:31 +0700

    back sone method version 0.0.1

--- MyProject.java
+++ MyProject.java
@@ -1,7 +1,12 @@
 package kasp.myproject;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
 public class MyProject {
+
     public static void main(String[] args) {
         System.out.println("Hello Wolrd");
         //Hello World Program

1253c2c92fa688e9e22f8d0cbae28a917a96c333
Kasidate P <bank@kasidate.me>
2022-03-25 10:17:56 +0700

    modified file version 0.1 helloworld

--- MyProject.java
+++ MyProject.java
@@ -1,14 +1,9 @@
 package kasp.myproject;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author User
- */
 public class MyProject {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello Wolrd");
+        //Hello World Program
+    }
 }

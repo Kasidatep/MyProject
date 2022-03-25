be57fd64c061b5111bd445dd60d917c34a4a52f5
Kasidate P <bank@kasidate.me>
2022-03-25 11:43:14 +0700

    version 0.1.1 hijava function

--- MyProject.java
+++ MyProject.java
@@ -5,5 +5,9 @@ public class MyProject {
     public static void main(String[] args) {
         System.out.println("Hello Wolrd");
         //Hello World Program
+        hijava();
+    }
+    public static void hijava(){
+        System.out.println("Hi Java");
     }
 }
